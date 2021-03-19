d3459b0147cd75d209b5ca7ccfebf93b648f2a88
mspruc <55182629+mspruc@users.noreply.github.com>
2021-03-19 13:02:42 +0100

    it is a commit but it does nothing i have issues with how to import data on line 24 in importer because i dont know how to create edge w. vertices :((((

--- src/Graph.java
+++ src/Graph.java
@@ -4,11 +4,10 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Graph {
-    Vertex[] graph;
+    Vertex[] vertices;
 
-    Graph () {
-
-
+    Graph (Vertex[] vertices) {
+        this.vertices = vertices;
     }
 
     ArrayList<Vertex> findMST (Vertex[] graph){
@@ -26,6 +25,15 @@ public class Graph {
 }
 
 class Edge implements Comparable<Edge>{
+    @Override
+    public String toString() {
+        return "Edge{" +
+                "source='" + source + '\'' +
+                ", destination='" + destination + '\'' +
+                ", distance=" + distance +
+                '}';
+    }
+
     String source;
     String destination;
     int distance;
@@ -42,14 +50,26 @@ class Edge implements Comparable<Edge>{
         else if (this.distance > o.distance) return 1;
         return 0;
     }
+
 }
 
 class Vertex{
     String name;
+
+    @Override
+    public String toString() {
+        return "Vertex{" +
+                "name='" + name + '\'' +
+                ", adjVertex=" + Arrays.toString(adjVertex) +
+                ", isReached=" + isReached +
+                '}';
+    }
+
     Edge[] adjVertex;
+    public boolean isReached = false;
 
     Vertex (String name, Edge[] adjVertices){
-        this.adjVertex = adjVertices.clone();
+        this.adjVertex = adjVertices;
         this.name = name;
     }
 }
--- src/Importer.java
+++ src/Importer.java
@@ -1,6 +1,9 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.lang.management.GarbageCollectorMXBean;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
@@ -10,6 +13,7 @@ public class Importer {
 
     public Importer(String fileName) {
         try {
+            HashSet<String> testSet = new HashSet<String>();
             PriorityQueue<Edge> test = new PriorityQueue<>();
             fileReader = new FileReader(fileName);
             this.fileName = fileName;
@@ -17,11 +21,22 @@ public class Importer {
             scanner.nextLine(); //skip first line :)
             while (scanner.hasNext()) {
                 String[] splitLine = scanner.nextLine().split(" ");
-
                 Edge edge = new Edge(splitLine[0],splitLine[1],Integer.parseInt(splitLine[2]));
 
+                testSet.add(splitLine[0]);
                 test.add(edge);
             }
+
+            ArrayList<Vertex> tempList = new ArrayList<>();
+
+            for (String string : testSet) {
+                Vertex vertex = new Vertex(string, null);
+                tempList.add(vertex);
+            }
+
+            //System.out.println(tempList.toString());
+            //System.out.println(test.peek());
+            //System.out.println(testSet.toString());
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             System.out.println("file could not be parsed");
